828e157757bc89b9328de11d1c68e5547327d17e
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-27 16:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 class Driver {
   constructor(name, startDate) {
     this.name = name;
-    this.startDate = new Date(startDate)
+    this.startDate = new Date(startDate);
     }
 
   startDate() {
     return Date()
+  }
+
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+    
   }
 }
